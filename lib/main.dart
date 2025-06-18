49b00caadc149745d97a6661df058b2cf9dce0ee
Alan Varghese <alanvz2212@example.com>
2025-06-18 16:02:29 +0530

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:journal_app/controllers/notification_service.dart';
 import 'package:journal_app/controllers/notification_task.dart';
+import 'package:journal_app/model/diary.dart';
+import 'package:journal_app/model/event.dart';
+import 'package:journal_app/model/moods.dart';
+import 'package:journal_app/model/user.dart';
+import 'package:journal_app/screens/splash_screen.dart';
 import 'package:workmanager/workmanager.dart';
 
 void main() async {
@@ -9,20 +14,32 @@ void main() async {
   await Hive.initFlutter();
   await NotificationService().init();
   Workmanager().initialize(callbackDispatcher, isInDebugMode: false);
-  
+
   Hive.registerAdapter(UserAdapter());
   Hive.registerAdapter(EventAdapter());
   Hive.registerAdapter(MoodDataAdapter());
   Hive.registerAdapter(DiaryAdapter());
 
+  await Hive.openBox<User>('users');
+  await Hive.openBox<Event>('events');
+  await Hive.openBox<MoodData>('moods');
+  await Hive.openBox<Diary>('journals');
+
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  late ThemeData _themeData;
+
+  MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(debugShowCheckedModeBanner: false, home: SplashScreen());
   }
 }
